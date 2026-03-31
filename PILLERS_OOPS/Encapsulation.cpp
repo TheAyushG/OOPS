a9100679d0f3fa89f79ab20e37c4f175833f96d1
Ayush Gurjar <jiaayush256@gmail.com>
2026-03-31 15:23:47 +0530

    Encapsulation.cpp

--- PILLERS_OOPS/Encapsulation.cpp
+++ PILLERS_OOPS/Encapsulation.cpp
@@ -1,16 +1,27 @@
-Abstraction means hiding internal implementation details and showing only the essential features to the user.
+Simple Interview Definition :-
+Encapsulation is the process of wrapping data (variables) and functions (methods) into a single unit (class) 
+and restricting direct access to the data.
 
 #include <iostream>
 using namespace std;
 
-class ATM{
-public:
-    void withdraw(){
-        cout<<"money withdraw"<<endl;
+class bank{
+    private:
+    int balance;
+
+    public:
+    void setBalance(int bal){
+        balance = bal;
+    }
+
+    int getBalance(){
+        return balance;
     }
 };
 
+
 int main(){
-    ATM a;
-    a.withdraw();
+    bank b1;
+    b1.setBalance(5000);
+    cout<<b1.getBalance();
 }
